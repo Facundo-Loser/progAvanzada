e44ece62b41402c4e3a819730831d503b14c3ea2
Loser Facundo Dario <loserfacundodario@gmail.com>
2023-04-13 14:29:36 -0300

    p5 actualizada

--- practica5.hs
+++ practica5.hs
@@ -10,6 +10,9 @@ pListNat :: Int -> [Int]
 pListNat 0 = []
 pListNat n = pListNat(n-1) ++ [n]
 
+primNat :: Int -> [Int]
+primNat n = [0..n]
+
 --Ejercicio 4:
 primCinco :: [Int] -> [Int]
 primCinco xs = aux xs 1
@@ -57,3 +60,85 @@ listSucc [] = []
 listSucc (x:xs) = (succ x):(listSucc xs)
 
 --Ejercicio 10:
+sumList :: [Int] -> Int
+sumList [] = 0
+sumList xs = foldr (+) 0 xs
+
+--Ejercicio 11:
+fact :: [Int] -> Int
+fact xs = foldr (*) 1 xs
+
+--Ejercicio 12:
+and' :: [Bool] -> Bool
+and' [] = False
+and' xs = foldl (&&) True xs
+
+--Ejercicio 13:
+tam :: [a] -> Int
+tam [] = 0
+tam xs = foldr (+) 0 (listUno xs)
+
+listUno :: [a] -> [Int]
+listUno [] = []
+listUno (x:xs) = 1:(listUno xs)
+
+--Otra forma:
+contar :: a -> Int -> Int --acc: acumulador
+contar _ acc = acc + 1
+
+tam' :: [a] -> Int
+tam' xs = foldr contar 0 xs
+
+--Ejercicio 14:
+sucesores :: [Int] -> [Int]
+sucesores [] = []
+sucesores xs = [succ x | x <- xs]
+
+--Ejercicio 15:
+cuadList' :: [Int] -> [Int]
+cuadList' [] = []
+cuadList' xs = [x^2 | x <- xs]
+
+--Ejercicio 16:
+listPares :: [Int] -> [Int]
+listPares [] = []
+listPares xs = [x | x <- xs, (mod x 2) == 0, x > 10]
+
+--Ejercicio 17:
+divs :: Int -> [Int]
+divs n = [x | x <- divisores n]
+
+divs' :: Int -> [Int]
+divs' n = [x | x <- [1..n], (mod n x) == 0]
+
+--Ejercicio 18:
+enLista :: (Eq a) => a -> [a] -> Bool
+enLista n [] = False
+enLista n (x:xs) = if n == x then True else enLista n xs
+
+todosOcurrenEn :: (Eq a) => [a] -> [a] -> Bool
+todosOcurrenEn [] [] = True
+todosOcurrenEn xs [] = False
+todosOcurrenEn [] ys = True
+todosOcurrenEn (x:xs) ys = if enLista x ys then True && (todosOcurrenEn xs ys) else False
+
+--Ejercicio 19:
+primo' :: Int -> Bool
+primo' 1 = False
+primo' n = and [mod n i /= 0 | i <- [2..(n-1)]]
+
+listaPrimos :: Int -> [Int]
+listaPrimos 0 = []
+listaPrimos x
+         | primo x == True = x:(listaPrimos (x-1))
+         | otherwise = listaPrimos (x-1) 
+
+--Función principal:
+primos :: Int -> [Int]
+primos n = [x | x <- listaPrimos n]
+
+--Otra Forma:
+primos' :: Int -> [Int]
+primos' n = [x | x <-[2..n], primo' x]
+
+--Ejercicio 20:
